aca41b440076136dc1da17b7b21ee6f23c65d3e0
Gersoncs107 <gerson.silva107@gmail.com>
2025-04-05 23:16:45 -0300

    gitAdd

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,11 @@ module.exports = {
       {
         test: /\.html$/i,
         loader: "html-loader",
-      }      
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
 };
